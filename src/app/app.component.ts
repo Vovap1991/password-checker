02a30867979ed96a848184f995520fc739001dfe
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-11-07 20:02:47 +0200

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+  
 export class AppComponent {
-  title = 'password-checker';
+  password: string = '';
+  passwordStrength: string = '';
+
+
+
+  checkPasswordStrength() {
+
+    const password = this.password.trim();
+
+    const hasLetters = /[a-zA-Z]/.test(this.password);
+    const hasNumbers = /[0-9]/.test(this.password);
+    const hasSymbols = /(?=.*[!@#$%^&*]).$/.test(this.password);
+
+    const easy = hasLetters || hasNumbers || hasSymbols;
+    const medium = (hasLetters && hasNumbers) || (hasLetters && hasSymbols) || (hasNumbers && hasSymbols);
+    const strong = hasLetters && hasNumbers && hasSymbols;
+
+if (password === '') {
+  this.passwordStrength = 'grey';
+} else if (password.length >=1 && password.length < 8) {
+  this.passwordStrength = 'red';
+} else if (strong) {
+  this.passwordStrength = 'strong';
+} else if (medium) {
+  this.passwordStrength = 'medium';
+} else if (easy) {
+  this.passwordStrength = 'easy';
+}
+  }
 }
